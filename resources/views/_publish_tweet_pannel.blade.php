449af82e99bf13a9657da5febedcd8cfaecc44be
meri12345 <meri_zverka@hotmail.com>
2020-08-13 13:07:34 +0200

    designing timeline

---
+++ resources/views/_publish_tweet_pannel.blade.php
@@ -0,0 +1,17 @@
+<div class="border border-blue-400 rounded-lg py-6 px-8 mb-8">
+    <form action="">
+                    <textarea
+                        name="body"
+                        class="w-full"
+                        placeholder="What's up?"></textarea>
+    </form>
+    <hr class="my-4">
+    <footer class="flex justify-between">
+        <img src="https://i.pravatar.cc/40"
+             alt=""
+             class="rounded-full mr-2">
+
+        <button class="bg-blue-500 shadow rounded-lg py-2 px-2 text-white" type="submit">Tweet</button>
+
+    </footer>
+</div>
