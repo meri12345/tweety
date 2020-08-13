82ddc78791161533043eaebc0edeb1efaed51556
meri12345 <meri_zverka@hotmail.com>
2020-08-13 14:38:42 +0200

    follow feature

--- app/User.php
+++ app/User.php
@@ -43,4 +43,12 @@ class User extends Authenticatable
     public function getAvatarAttribute(){
         return 'https://i.pravatar.cc/40?u='. $this->email;
     }
+
+    public function follow(User $user){
+        return $this->follow()->save($user);
+    }
+
+    public function follows(){
+        return $this->belongsToMany(User::class,'follows','user_id','following_user_id');
+    }
 }
